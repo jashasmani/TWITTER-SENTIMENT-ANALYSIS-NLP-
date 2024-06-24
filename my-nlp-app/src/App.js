e9caaded16ca9873867601d71c2d5f2ac947a0fa
jash asmani <asmanijash51@gmail.com>
2024-06-24 18:40:58 +0530

    retrive

--- my-nlp-app/src/App.js
+++ my-nlp-app/src/App.js
@@ -1,34 +1,73 @@
-// import React from "react";
-// import Home from "./Components/Home";
+import React, { useState } from "react";
+import axios from "axios";
+import "./App.css";
 
-// function App() {
-//   return (
-//     <>
-//       <Home />
-//     </>
-//   );
-// }
+function App() {
+  const [text, setText] = useState("");
+  const [prediction, setPrediction] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-// export default App;
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(false);
+    try {
+      const response = await axios.post("http://localhost:5000/predict", {
+        text,
+      });
+      setPrediction(response.data.prediction);
+      setLoading(false);
+    } catch (error) {
+      console.error("There was an error making the request!", error);
+      setError(true);
+      setLoading(false);
+    }
+  };
 
-
-// App.js
-
-import React from 'react';
-import Post from './Components/Post'; 
-import './App.css'
-
-const App = () => {
   return (
-    <div className="app">
-      <Post
-        username="Jash Asmani"
-        imageUrl="https://ceotoday.co/wp-content/uploads/2023/04/3-wrd.jpg"
-        likes={123}
-        caption="This is a sample caption."
-      />
+    <div className="App">
+      <div className="container">
+        <div style={{ margin: "1.5rem" }}>
+          <span
+            style={{
+              fontFamily: "Montserrat",
+              fontWeight: 600,
+              fontSize: "1.5rem",
+            }}
+          >
+            Twitter Sentiment Analysis with NLP
+          </span>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <textarea
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter Comment..."
+            className="input-text"
+          />
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+        <p className="prediction">
+          {loading ? (
+            <span>Loading...</span>
+          ) : prediction === 0 ? (
+            <span style={{ color: "Red" }}>Negative Comment</span>
+          ) : (
+            <span style={{ color: "Green" }}>Positive Comment</span>
+          )}
+        </p>
+        <p className="error">
+          {error ? (
+            <span>Error: Please check your input and try again.</span>
+          ) : null}
+        </p>
+      </div>
     </div>
   );
-};
+}
 
 export default App;
